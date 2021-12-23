b2f67a5c1dab00ea31f02825dc86c739ca965bba
Oluwademilade Dammy Adisa <84209391+DemiAdisa@users.noreply.github.com>
2021-12-23 06:46:17 -0400

    Time: 0 ms (100.00%), Space: 38.2 MB (99.25%) - LeetHub

---
+++ richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        
+        int maxWealth = 0;
+        int sum = 0;
+        for(int i = 0 ; i < accounts.length; i++)
+        {
+            for(int j = 0; j < accounts[i].length; j++)
+            {
+                sum += accounts[i][j];
+                
+            }
+            
+            if(sum > maxWealth)
+            {
+                maxWealth = sum;
+                sum = 0;
+            }else
+            {
+                sum = 0;
+            }
+        }
+        
+        return maxWealth;
+    }
+}
